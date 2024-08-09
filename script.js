d73a815bd7f6c2d44264b7b3062ea50713e02003
Ndzi Nfor <ndzinfor@gmail.com>
2024-08-09 15:47:04 +0100

    process fetched data

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-async function fetchData(location){
-    let city = location
+async function fetchData(city){
     let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=Y42383TB7X23CAJJWBPFX6DWE`;
     try {
         let response = await fetch(url, { mode: 'cors' });
@@ -8,10 +7,16 @@ async function fetchData(location){
             throw new Error('Network response was not ok');
         }
         let data = await response.json();
-        console.log(data); // Return the fetched data
+        //console.log(data); // Return the fetched data
+        processData(data);
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
 }
-fetchData('london');
+
+
+function processData(data){
+    console.log(data.days)
+}
+
 
